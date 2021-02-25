70b9f6a67f3d6a48f109a9ef23d67017efc1e32d
Martin <martin.knutsen@live.no>
2021-02-26 00:24:43 +0100

    You can try Enemy Objects now.
    I'll hopefully be able to implement sprite changing some time soon, Tried for a while now and can't get it to work...

--- Assets/Scripts/ScriptableObjects/Dialogue/Scripts/TextLoader.cs
+++ Assets/Scripts/ScriptableObjects/Dialogue/Scripts/TextLoader.cs
@@ -1,78 +1,78 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Dialogue.Scripts;
+﻿using Dialogue.Scripts;
 using TMPro;
 using UnityEngine;
 
-public class TextLoader : MonoBehaviour
+namespace ScriptableObjects.Dialogue.Scripts
 {
-    public Dialog dialogue;
-    private Response[] responses;
-    
-    private string name;
-    private string npcText;
-    private string[] responseText;
-    private int importantInt;
-    
-    public TMP_Text tmp_npcName;
-    public TMP_Text tmp_npcText;
-    public TMP_Text tmp_response1;
-    public TMP_Text tmp_response2;
-    public TMP_Text tmp_Player;
-    public TMP_Text stats;
-
-    public void Update()
+    public class TextLoader : MonoBehaviour
     {
-        responses = dialogue.message.responseOptions;
+        public Dialog dialogue;
+        private Response[] responses;
+    
+        private string name;
+        private string npcText;
+        private string[] responseText;
+        private int importantInt;
+    
+        public TMP_Text tmpNpcName;
+        public TMP_Text tmpNpcText;
+        public TMP_Text tmpResponse1;
+        public TMP_Text tmpResponse2;
+        public TMP_Text tmpPlayer;
+        public TMP_Text stats;
 
-        tmp_response1.text = responses[0].responseText;
-        tmp_response2.text = responses[1].responseText;
+        public void Update()
+        {
+            responses = dialogue.message.responseOptions;
 
-        name = dialogue.npcName;
-        npcText = dialogue.message.message;
+            tmpResponse1.text = responses[0].responseText;
+            tmpResponse2.text = responses[1].responseText;
+
+            name = dialogue.npcName;
+            npcText = dialogue.message.message;
         
-        tmp_npcName.text = name;
-        tmp_npcText.text = npcText;
+            tmpNpcName.text = name;
+            tmpNpcText.text = npcText;
         
-        stats.text = "Strength " + dialogue.number1 + " Health " + dialogue.number2 + " Stanima " + dialogue.number3;
+            stats.text = "Strength " + dialogue.number1 + " Health " + dialogue.number2 + " Stanima " + dialogue.number3;
 
-        if (dialogue != dialogue)
-        { 
-            tmp_npcName.gameObject.SetActive(false);
-            tmp_npcText.gameObject.SetActive(false);
-            tmp_response1.gameObject.SetActive(false);
-            tmp_response2.gameObject.SetActive(false);
-            tmp_Player.gameObject.SetActive(false);
-            stats.gameObject.SetActive(false);
+            if (dialogue != dialogue)
+            { 
+                tmpNpcName.gameObject.SetActive(false);
+                tmpNpcText.gameObject.SetActive(false);
+                tmpResponse1.gameObject.SetActive(false);
+                tmpResponse2.gameObject.SetActive(false);
+                tmpPlayer.gameObject.SetActive(false);
+                stats.gameObject.SetActive(false);
+            }
         }
-    }
 
-    public void Click(int buttonNumber)
-    {
-        int importantInt = buttonNumber;
-        // dialogue = responses[importantInt].response;
+        public void Click(int buttonNumber)
+        {
+            int importantInt = buttonNumber;
+            // dialogue = responses[importantInt].response;
         
-        dialogue = responses[importantInt].response;
+            dialogue = responses[importantInt].response;
         
-        LoadText(dialogue);
-        tmp_Player.text = responses[buttonNumber].responseText;
+            LoadText(dialogue);
+            tmpPlayer.text = responses[buttonNumber].responseText;
 
-    }
+        }
 
-    public void LoadText(Dialog dialog)
-    {
-        name = dialogue.npcName;
-        npcText = dialogue.message.message;
+        public void LoadText(Dialog dialog)
+        {
+            name = dialogue.npcName;
+            npcText = dialogue.message.message;
         
-        tmp_npcName.text = name;
-        tmp_npcText.text = npcText;
+            tmpNpcName.text = name;
+            tmpNpcText.text = npcText;
         
-    }
+        }
 
-    public void ShowText()
-    {
+        public void ShowText()
+        {
         
-    }
+        }
     
+    }
 }
