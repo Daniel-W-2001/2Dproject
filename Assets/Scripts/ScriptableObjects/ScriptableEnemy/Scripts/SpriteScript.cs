de6bc75dd900afad7ee806edb2aa926a6cb776d8
Martin <martin.knutsen@live.no>
2021-03-04 00:24:23 +0100

    Some scripts were doubled, had to manually pick and delete, hope everything ended up ok.

---
+++ Assets/Scripts/ScriptableObjects/ScriptableEnemy/Scripts/SpriteScript.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Sprites;
+using UnityEngine;
+
+public class SpriteScript : MonoBehaviour
+{
+    [NonSerialized]
+    public int spritesAmount = 17;
+    public Sprite[] sprites;
+}
