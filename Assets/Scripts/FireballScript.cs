2e68e95ef12ea4b196507ceda968629738a0cb06
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2021-03-03 19:14:29 +0100

    Fireball & casting anim, impact anim. Shoot with M

---
+++ Assets/Scripts/FireballScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects.ScriptableEnemy.Scripts;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    public float speed = 13f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject explosion;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy2 enemy = hitInfo.GetComponent<Enemy2>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            var boom = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(boom, 0.5f);
+        }
+    }
+}
