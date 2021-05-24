92817ee249969ddfaab7beb39ec38acd8f51fb25
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2021-05-24 15:22:11 +0200

    Static Camera Rooms

---
+++ Assets/Scripts/Camera/StaticCam.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCam : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") Camera.main.transform.position = transform.position + new Vector3(0, 0, -10);
+    }
+}
