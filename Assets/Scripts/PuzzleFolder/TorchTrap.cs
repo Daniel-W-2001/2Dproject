a9bc3fb9f6c4fa7d593c7fe7b6240f922885109b
Martin <martin.knutsen@live.no>
2021-05-31 18:19:36 +0200

    Chandeleir style witch boss

--- Assets/Scripts/PuzzleFolder/TorchTrap.cs
+++ Assets/Scripts/PuzzleFolder/TorchTrap.cs
@@ -1,27 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TorchTrap : MonoBehaviour
 {
-    public Torch torchScript;
-    public Torch torchScript2;
-
-    public GameObject fireballs;
+    [Header("TorchObjects")]
+    public Torch[] torches;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
+    [Header("What will the torch shoot?")]
+    public GameObject fireball;
+    
+    [SerializeField]private int ignitedCount = 0;
+    
     // Update is called once per frame
     void Update()
     {
-        if (torchScript.ignited && torchScript2.ignited)
+        if (torches[ignitedCount].ignited)
         {
-            Instantiate(fireballs, gameObject.transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            ignitedCount++;
+            
+            if (ignitedCount == torches.Length) {
+                Instantiate(fireball, gameObject.transform.position, Quaternion.identity);
+                gameObject.SetActive(false);
+            }
         }
+        
+        
     }
     
 }
