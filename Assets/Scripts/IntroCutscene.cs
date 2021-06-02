f77395d21d9ce8c148e5a939106b370a1415b65f
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2021-06-02 12:06:10 +0200

    Intro Scene with Cam_Anim & FadetoBlack

---
+++ Assets/Scripts/IntroCutscene.cs
@@ -0,0 +1,55 @@
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroCutscene : MonoBehaviour
+{
+    float timeLeft = 22f;
+    public GameObject blackOutSquare;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            StartCoroutine(FadeBlackOutSquare());
+            Invoke("LoadScene", 2);
+        }
+    }
+
+    void LoadScene()
+    {
+            SceneManager.LoadScene("Kasper");
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 5)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+        else
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+    }
+}
