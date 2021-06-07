d634a36082861bd97487ffe16aaae3e714c958af
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2021-06-07 16:30:16 +0200

    Gameover now resets lifecounter

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    void Start()
+    {
+        LifeCount.lifeCount = 5;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("Intro");
